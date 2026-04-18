636e02734e36c8c3035b588a8cc06aed26c46418
Giselle Oviedo <giselle@oviedo.com>
2026-04-18 12:42:48 -0300

    error

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -251,7 +251,6 @@ class PropertyController extends Controller
         $data['slug'] = Str::slug($data['name']);
         $data['rules'] = $request->filled('rules') ? explode("\n", $request->rules) : null;
         $data['status'] = 'active';
-        $data['min_days'] = $data['min_days'] ?? 1;
         $this->filterDiscounts($data);
 
         $propiedad = Property::create($data);
@@ -380,7 +379,6 @@ class PropertyController extends Controller
         $data['city'] = $data['locality'];
         $data['country'] = $data['country'] ?? 'Argentina';
         $data['rules'] = $request->filled('rules') ? explode("\n", $request->rules) : null;
-        $data['min_days'] = $data['min_days'] ?? 1;
         $this->filterDiscounts($data);
 
         // Log newly added custom amenities
